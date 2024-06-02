f7ef7ea160a925adcdb811070c770615a8b44b4d
Nathan Perez <nathanperez@pop-os.attlocal.net>
2024-06-02 08:31:17 -0500

    changed cpu to register.rs

---
+++ src/cpu/register.rs
@@ -0,0 +1,163 @@
+pub enum Register8 {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    H,
+    L,
+}
+
+pub enum Register16 {
+    AF,
+    BC,
+    DE,
+    HL,
+    SP,
+    PC,
+}
+
+// all registars are 8 bit unsigned ints
+#[derive(Debug)]
+pub struct RegisterData {
+    a: u8,            // accumulator registar
+    b: u8,            // 8 bit registar
+    c: u8,            // 8 bit registar
+    d: u8,            // 8 bit registar 
+    e: u8,            // 8 bit registar
+    f: FlagsRegister, // flag registar
+    h: u8,            // 8 bit registar
+    l: u8,            // 8 bit registar
+    sp: u16,          // stack pointer
+    pc: u16,          // program counter
+}
+
+// 16 bit instructions use af, bc, de, and hl combonied to store 16 bits
+impl RegisterData {   
+    pub fn new() -> RegisterData {
+        RegisterData {
+            a: 0, 
+            b: 0,
+            c: 0,
+            d: 0,
+            e: 0,
+            f: FlagsRegister {zero: false, subtract: false, half_carry: false, carry: false},
+            h: 0,
+            l: 0,
+            sp: 0xFFFE,
+            pc: 0x000,
+        }
+    }
+    
+    // reads as u8 register info
+    pub fn read_8(&self, reg: Register8) -> u8 {
+        use self::Register8::*;
+        match reg {
+            A => self.a,
+            B => self.b,
+            C => self.c,
+            D => self.d,
+            E => self.e,
+            F => u8::from(&self.f),
+            H => self.h,
+            L => self.l,
+        }
+    }
+    
+    // reads as u16 register info
+    pub fn read_u16(&self, reg: Register16) -> u16 {
+        use self::Register16::*;
+        match reg { 
+           AF => (self.a as u16) << 8 | (u8::from(&self.f) as u16),
+           BC => (self.b as u16) << 8 | (self.c as u16),
+           DE => (self.d as u16) << 8 | (self.e as u16), 
+           HL => (self.h as u16) << 8 | (self.l as u16), 
+           SP => self.sp,
+           PC => self.pc,
+        }
+    }
+
+    pub fn write_u8(&mut self, reg: Register8, value: u8) {
+        use self::Register8::*;
+        match reg {
+           A => self.a = value, 
+           B => self.b = value, 
+           C => self.c = value, 
+           D => self.d = value, 
+           E => self.e = value, 
+           F => self.f = value.into(), 
+           H => self.h = value, 
+           L => self.l = value, 
+        }
+    } 
+
+    pub fn write_u16(&mut self, reg: Register16, value: u16) {
+
+        let hi: u8 = (value >> 8) as u8; 
+        let lo: u8 = (0x00FF & value) as u8;
+        
+        use self::Register16::*;
+        match reg {
+            AF => {
+                self.a = hi;
+                self.f = lo.into();
+            },
+            BC => {
+                self.b = hi;
+                self.c = lo;
+            },
+            DE => {
+                self.d = hi;
+                self.e = lo;
+            },
+            HL => {
+                self.h = hi;
+                self.e = lo;
+            },
+            SP => self.sp = value,
+            PC => self.pc = value,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct FlagsRegister {
+    zero: bool,
+    subtract: bool,
+    half_carry: bool,
+    carry: bool,
+}
+
+// const for the bits locations in the byte, lower 4 are always 0
+const ZERO_FLAG_BYTE_POSITION: u8 = 7;
+const SUBTRACT_FLAG_BYTE_POSTION: u8 = 6;
+const HALF_CARRY_FLAG_BYTE_POSITION: u8 = 5;
+const CARRY_FLAG_BYTE_POSITION: u8 = 4;
+
+// from flag to 8 bit unsigned
+impl std::convert::From<&FlagsRegister> for u8 {
+    fn from(flag: &FlagsRegister) -> u8 {
+        (if flag.zero        {1} else {0}) << ZERO_FLAG_BYTE_POSITION |
+        (if flag.subtract    {1} else {0}) << SUBTRACT_FLAG_BYTE_POSTION |
+        (if flag.half_carry  {1} else {0}) << HALF_CARRY_FLAG_BYTE_POSITION |
+        (if flag.carry       {1} else {0}) << CARRY_FLAG_BYTE_POSITION
+    }
+}
+
+// from 8 bit unsigned to flag
+impl std::convert::From<u8> for FlagsRegister {
+    fn from(byte: u8) -> Self {
+        let zero = ((byte >> ZERO_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let subtract = ((byte >> SUBTRACT_FLAG_BYTE_POSTION) & 0b1) != 0;
+        let half_carry = ((byte >> HALF_CARRY_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let carry = ((byte >> CARRY_FLAG_BYTE_POSITION) & 0b1) != 0;
+
+        FlagsRegister {
+            zero,
+            subtract,
+            half_carry,
+            carry
+        }
+    }
+}
