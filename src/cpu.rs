04a0127b8e08e366d844b99fa356598730d49924
Nathan Perez <nathanperez@pop-os.attlocal.net>
2024-06-05 11:56:28 -0500

    added constructor and opcode to CPU

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,6 +1,19 @@
 mod register;
 mod decode;
+mod execute;
+
+use crate::cpu::register::{Register8, Register16, RegisterData};
 
 pub struct Cpu {
-    pub registers: register::RegisterData
+    pub registers: register::RegisterData,
+    pub opcode: u8,
+}
+
+impl Cpu {
+    pub fn new() -> Cpu {
+        Cpu {
+            registers: RegisterData::new(),
+            opcode: 0x00,
+        }
+    }
 }
