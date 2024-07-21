dd4e81787c57c648dc000b2e9de47201546227b2
NathanPerezR <nathanperez.business@gmail.com>
2024-07-21 10:41:58 -0500

    general testing stuff

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-mod cpu;
+mod cart;
+use crate::cart::Cart;
 
 fn main() {
-  
+    let mut cart: Cart = Cart::default();
+
+    cart.cart_read("/Users/nathanperez/repo/GameBoy-Emulator/Roms/Vegas-Stakes.gb");
 }
