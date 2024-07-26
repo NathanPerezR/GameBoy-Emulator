bb283ff72110f84e803d686ab4870948e5e4e0cd
NathanPerezR <nathanrp62@gmail.com>
2024-07-25 21:10:35 -0500

    cpu started

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,32 +1,110 @@
-mod register;
 mod decode;
 mod execute;
+mod register;
+use crate::cpu::register::{Register8, Register16};
+use crate::bus::bus_read;
+use crate::cart::Cart;
 
-use crate::cpu::register::{Register5, Register16, RegisterData};
+#[derive(Debug)]
+enum AddressMode{
+    AmImp,
+    AmRD16,
+    AmRR,
+    AmMrR,
+    AmR,
+    AmRD8,
+    AmRMr,
+    AmRHli,
+    AmRHld,
+    AmHliR,
+    AmHldR,
+    AmRA8,
+    AmA8R,
+    AmHlSpr,
+    AmD16,
+    AmD8,
+    AmD16R,
+    AmMrD8,
+    AmMr,
+    AmA16R,
+    AmRA16,
+}
 
-pub struct Cpu {
+enum ConditionType {
+    CtNone,
+    CtNz,
+    CtZ,
+    CtNc,
+    CtC,
+}
+
+enum RegisterType {
+    Register8(Register8), 
+    Register16(Register16),
+}
+
+struct Cpu {
     pub registers: register::RegisterData,
-    pub opcode: u8,
+    pub cpu_ctx: CpuContext,
 }
 
-pub enum Step {
-    Running,
-    Halt,
-    InterruptDispatch,
+struct CpuContext {
+    fetched_date: u16,
+    mem_dest: u16,
+    dest_is_mem: u16,
+    current_opcode: u8,
+    current_instruction: Instruction,
+    halted: bool, 
+    stepping: bool,
+    int_master_enabled: bool,
 }
+
+struct Instruction {
+    in_type: u8,
+    mode: AddressMode, 
+    register_1: AddressMode, 
+    register_2: AddressMode,
+    condition: ConditionType, 
+    parmater: u8,
+} 
 
 impl Cpu {
-    pub fn new() -> Cpu {
-        Cpu {
-            registers: RegisterData::new(),
-            opcode: 0x00,
+    
+    fn fetch_instruction(&mut self, cart: Cart) {
+        self.cpu_ctx.current_opcode = bus_read(cart , self.registers.pc); 
+        self.cpu_ctx.current_instruction = instruction_by_opcode(self.cpu_ctx.current_opcode);
+    }
+
+    fn fetch_data(& self) {
+
+    }
+
+    fn execute() {
+
+    }
+
+    fn cpu_step(&mut self, cart: Cart) -> bool {
+        
+        if !self.cpu_ctx.halted {
+            let pc: u16 = self.registers.pc;
+
+            self.fetch_instruction();
+            self.fetch_data();
+
+            println!("{:04X}: {:<7} ({:02X} {:02X} {:02X}) A: {:02X} B: {:02X} C: {:02X}", 
+                     self.registers.pc,
+                     5,
+                     self.current_opcode,
+                     bus_read(cart, self.registers.pc + 1),
+                     bus_read(cart, self.registers.pc + 2),
+                     self.registers.a,
+                     self.registers.b,
+                     self.registers.c,
+                    );
+            
+            self.execute();
         }
-    }
 
-    pub fn step(&mut self) -> bool {
-    }
-
-    pub fn init(&mut self) {
-
+        true
     }
 }
