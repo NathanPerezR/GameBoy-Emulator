29ad8368252b645a4216865c8065387bde232ee8
NathanPerezR <nathanrp62@gmail.com>
2024-07-31 10:34:24 -0500

    changed bit_set to work with u8

--- src/util.rs
+++ src/util.rs
@@ -7,14 +7,17 @@ pub fn nth_bit(input: u32, nth_bit: u32) -> bool {
     }
 }
 
-pub fn bit_set(input: &mut u32, nth_bit: u32, on: bool) {
+pub fn bit_set(mut input: u8, nth_bit: u8, on: bool) -> u8 {
     if on {
-        *input |= 1 << nth_bit;
+        input |= 1 << nth_bit;
     }
     else {
-        *input &= !(1<<nth_bit);
+        input &= !(1<<nth_bit);
     }
+    return input;
 }
+
+
 
 pub fn between(a: i32, b: i32, c: i32) -> bool {
     a >= b && a <= c
