2ee926e406d1b88e7e6fedf3fb12c533f38b80a1
nathanperezr <nathanrp62@gmail.com>
2024-08-04 15:32:40 -0700

    String for ROM now grabbed at program runtime

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,32 @@ mod bus;
 mod util;
 mod cpu;
 mod emu;
+use std::env;
+use std::path::Path;
 
 fn main() {
     let emu: emu::Emu = emu::Emu::default();
-    emu.emu_run("../Roms/dmg-acid2.gb");
+
+    // Get the command-line arguments
+    let args: Vec<String> = env::args().collect();
+
+    // Check if a file name is provided
+    if args.len() != 2 {
+        eprintln!("Usage: cargo run <File Name>");
+        return;
+    }
+
+    // Extract the file name
+    let file_name = &args[1];
+
+    // Construct the file path
+    let rom_path = Path::new("../Roms").join(file_name);
+
+    // Check if the file exists
+    if !rom_path.exists() {
+        eprintln!("Error: The file '{}' does not exist.", rom_path.display());
+        return;
+    }
+
+    emu.emu_run("../Roms/Tetris.gb");
 }
