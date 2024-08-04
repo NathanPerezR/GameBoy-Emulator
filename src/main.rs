028161b86dbef99bc6fabce37d09ecf22938ef20
nathanperezr <nathanrp62@gmail.com>
2024-08-03 18:02:39 -0700

    changed to relitive path

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,5 @@ mod emu;
 
 fn main() {
     let emu: emu::Emu = emu::Emu::default();
-    emu.emu_run("/home/nathanperez/Repo/GameBoy-Emulator/Roms/dmg-acid2.gb");
+    emu.emu_run("../Roms/dmg-acid2.gb");
 }
