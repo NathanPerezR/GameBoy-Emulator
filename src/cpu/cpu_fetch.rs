a312d74e3560b37ce38cb7f225d0842d779ddd05
NathanPerezR <nathanrp62@gmail.com>
2024-08-08 17:59:05 -0500

    fixed under & overflow issues with wrapping adds and subtracts

--- src/cpu/cpu_fetch.rs
+++ src/cpu/cpu_fetch.rs
@@ -18,7 +18,7 @@ impl Cpu {
             RD8 => {
                 self.cpu_ctx.fetched_data = bus.read(self.registers.pc) as u16;
                 self.emu_cycles(1);
-                self.registers.pc += 1;
+                self.registers.pc = self.registers.pc.wrapping_add(1);
             },
             RD16 | D16 => {
                 let lo: u8 = bus.read(self.registers.pc);
@@ -26,7 +26,7 @@ impl Cpu {
                                                                                                           
                 let hi: u8 = bus.read(self.registers.pc + 1);
                 self.emu_cycles(1);
-                self.registers.pc += 2;
+                self.registers.pc = self.registers.pc.wrapping_add(2);
                                                                                                           
                 self.cpu_ctx.fetched_data = ((hi as u16) << 8) | (lo as u16); 
             },
@@ -49,25 +49,25 @@ impl Cpu {
             RHli => {
                 self.cpu_ctx.fetched_data = bus.read(self.registers.read(self.cpu_ctx.instruction.register_2)) as u16;
                 self.emu_cycles(1);
-                self.registers.set_reg(RegisterType::HL, self.registers.read(RegisterType::HL) + 1)
+                self.registers.set_reg(RegisterType::HL, self.registers.read(RegisterType::HL).wrapping_add(1))
                 
             },
             RHld => {
                 self.cpu_ctx.fetched_data = bus.read(self.registers.read(self.cpu_ctx.instruction.register_2)) as u16;
                 self.emu_cycles(1);
-                self.registers.set_reg(RegisterType::HL, self.registers.read(RegisterType::HL) - 1);
+                self.registers.set_reg(RegisterType::HL, self.registers.read(RegisterType::HL).wrapping_sub(1));
             },
             HliR => {
                 self.cpu_ctx.fetched_data = self.registers.read(self.cpu_ctx.instruction.register_2);
                 self.cpu_ctx.mem_dest = self.registers.read(self.cpu_ctx.instruction.register_1);
                 self.cpu_ctx.dest_is_mem = true;
-                self.registers.set_reg(RegisterType::HL, self.registers.read(RegisterType::HL) + 1);
+                self.registers.set_reg(RegisterType::HL, self.registers.read(RegisterType::HL).wrapping_add(1));
             },
             HldR => {
                 self.cpu_ctx.fetched_data = self.registers.read(self.cpu_ctx.instruction.register_2);
                 self.cpu_ctx.mem_dest = self.registers.read(self.cpu_ctx.instruction.register_1);
                 self.cpu_ctx.dest_is_mem = true;
-                self.registers.set_reg(RegisterType::HL, self.registers.read(RegisterType::HL) - 1)
+                self.registers.set_reg(RegisterType::HL, self.registers.read(RegisterType::HL).wrapping_sub(1))
             },
             RA8 => {
                 self.cpu_ctx.fetched_data = bus.read(self.registers.pc) as u16;
