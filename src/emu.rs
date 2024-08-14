30a467b3c5043e124561e58739c6710ff071d1f2
NathanPerezR <nathanrp62@gmail.com>
2024-08-14 12:38:34 -0500

    reworked to be multi threaded

--- src/emu.rs
+++ src/emu.rs
@@ -1,13 +1,17 @@
 use crate::cpu::Cpu;
 use crate::bus::Bus;
+use std::sync::{Arc, Mutex, atomic::{AtomicBool, Ordering}};
+use std::time::Duration;
+use std::thread;
 
 pub struct EmuContext {
     pub running: bool,
     pub paused: bool,
     pub ticks: u64,
+    pub die: bool,
 
     pub cpu: Cpu,
-    pub bus: Bus, 
+    pub bus: Bus,
 }
 
 impl Default for EmuContext {
@@ -16,6 +20,7 @@ impl Default for EmuContext {
             running: true,
             paused: false,
             ticks: 0,
+            die: false,
             cpu: Cpu::default(),
             bus: Bus::default(),
         } 
@@ -25,28 +30,58 @@ impl Default for EmuContext {
 impl EmuContext {
 
     pub fn delay(&self, ms_int: u64) {
-        let ms_time = std::time::Duration::from_millis(ms_int);
+        let ms_time = Duration::from_millis(ms_int);
         std::thread::sleep(ms_time);
     }
 
-    pub fn emu_run(&mut self, rom_path: &str) -> i16 {
 
-        self.bus.cart.cart_load(rom_path);
+    pub fn cpu_run(&mut self) {
+        self.running = true;
+        self.paused = false;
+        self.ticks = 0;
+
         while self.running {
-           
             if self.paused {
                 self.delay(10);
                 continue;
             }
-
             if !self.cpu.step(&mut self.bus) {
-                print!("CPU STOPPED");
-                return -1;
-            };
-
+                println!("CPU Stopped");
+                return;
+            }
             self.ticks += 1;
-
         }
+    }
+
+    pub fn emu_run(emu_context: Arc<Mutex<EmuContext>>, rom_path: &str, stop_flag: Arc<AtomicBool>) -> i16 {
+        let mut emu = emu_context.lock().unwrap();
+        
+        if !emu.bus.cart.cart_load(rom_path) {
+            println!("Failed to load ROM file: {}", rom_path);
+            return -1;
+        }
+
+        let stop_flag_clone = Arc::clone(&stop_flag);
+        let emu_context_clone = Arc::clone(&emu_context);
+
+        let cpu_thread = thread::spawn(move || {
+            let mut emu = emu_context_clone.lock().unwrap();
+            while !stop_flag_clone.load(Ordering::Relaxed) {
+                emu.cpu_run();
+            }
+        });
+
+        drop(emu); // Release the lock on emu_context before the main thread continues
+
+        while !stop_flag.load(Ordering::Relaxed) {
+            thread::sleep(Duration::from_millis(1000));
+            // UI HERE
+        }
+
+        stop_flag.store(true, Ordering::Relaxed);
+        cpu_thread.join().unwrap();
+
         0
     }
+
 }
