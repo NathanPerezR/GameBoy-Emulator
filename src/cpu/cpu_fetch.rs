1303047b489219a8f52e618077da3fc58614b04e
NathanPerezR <nathanrp62@gmail.com>
2024-08-18 16:24:56 -0500

    fixed RR

--- src/cpu/cpu_fetch.rs
+++ src/cpu/cpu_fetch.rs
@@ -14,7 +14,7 @@ impl Cpu {
                 self.cpu_ctx.fetched_data = self.read(self.cpu_ctx.instruction.register_1);
             },
             RR => {
-                self.cpu_ctx.fetched_data = self.read(self.cpu_ctx.instruction.register_1);
+                self.cpu_ctx.fetched_data = self.read(self.cpu_ctx.instruction.register_2);
             }
             RD8 => {
                 self.cpu_ctx.fetched_data = bus.read(self.pc, *self) as u16;
@@ -73,23 +73,23 @@ impl Cpu {
             RA8 => {
                 self.cpu_ctx.fetched_data = bus.read(self.pc, *self) as u16;
                 self.emu_cycles(1);
-                self.pc += 1;
+                self.pc = self.pc.wrapping_add(1);
             }, 
             A8R => {
-                self.cpu_ctx.mem_dest = bus.read(self.pc, *self) as u16;
+                self.cpu_ctx.mem_dest = (bus.read(self.pc, *self) as u16) | 0xFF00;
                 self.cpu_ctx.dest_is_mem = true;
                 self.emu_cycles(1);
-                self.pc += 1;
+                self.pc = self.pc.wrapping_add(1);
             },
             HlSpr => {
                 self.cpu_ctx.fetched_data = bus.read(self.pc, *self) as u16;
                 self.emu_cycles(1);
-                self.pc += 1;
+                self.pc = self.pc.wrapping_add(1);
             },
             D8 => {
                 self.cpu_ctx.fetched_data = bus.read(self.pc, *self) as u16;
                 self.emu_cycles(1);
-                self.pc += 1;
+                self.pc = self.pc.wrapping_add(1);
             },
             A16R | D16R => {   
                 let lo: u16 = bus.read(self.pc, *self) as u16;
@@ -108,6 +108,7 @@ impl Cpu {
                 self.cpu_ctx.mem_dest = self.read(self.cpu_ctx.instruction.register_1);
                 self.cpu_ctx.dest_is_mem = true;
                 self.cpu_ctx.fetched_data = bus.read(self.read(self.cpu_ctx.instruction.register_1), *self) as u16;
+                self.emu_cycles(1)
             },
             RA16 => {
                 let lo: u16 = bus.read(self.pc, *self) as u16;
@@ -119,7 +120,7 @@ impl Cpu {
                 let address: u16 = lo | (hi << 8);
                 self.cpu_ctx.dest_is_mem = true;
 
-                self.pc += 2;
+                self.pc = self.pc.wrapping_add(2);
                 self.cpu_ctx.fetched_data = bus.read(address, *self) as u16;
                 self.emu_cycles(1);
             },
