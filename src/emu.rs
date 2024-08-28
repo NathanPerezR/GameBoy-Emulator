72204046f51d7f22804817efed9bb6e28fca8d55
NathanPerezR <nathanrp62@gmail.com>
2024-08-28 14:58:59 -0500

    put CPU and ui in seprate thread

--- src/emu.rs
+++ src/emu.rs
@@ -4,6 +4,7 @@ use crate::bus::Bus;
 use crate::ui::Ui;
 use std::time::Duration;
 use std::thread;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 pub struct EmuContext {
     pub running: bool,
@@ -41,7 +42,6 @@ impl EmuContext {
         self.running = true;
         self.paused = false;
         self.ticks = 0;
-        let mut ui = Ui::new(800, 800).expect("Failed to initialize UI");
 
         while self.running {
             if self.paused {
@@ -50,8 +50,7 @@ impl EmuContext {
             }
 
             let step_result = {
-                ui.handle_events();
-                ui.render();
+
                 self.cpu.step(&mut self.bus, &mut self.dbg)
             };
 
@@ -64,16 +63,23 @@ impl EmuContext {
         }
     }
 
-    pub fn emu_run(&mut self, rom_path: &str) -> i16 {
+    pub fn emu_run(mut self, rom_path: &str) -> i16 {
 
         if !self.bus.cart.cart_load(rom_path) {
             println!("Failed to load ROM file: {}", rom_path);
             return -1;
         }
 
+        let mut ui = Ui::new(800, 800).expect("Failed to initialize UI");
 
-        self.cpu_run();
-        thread::sleep(Duration::from_millis(10));
-        0
+        thread::spawn(move|| {
+            self.cpu_run();
+        });
+
+        loop {
+            ui.handle_events();
+            ui.render();
+            thread::sleep(Duration::from_millis(100));
+        }
     }
 }
